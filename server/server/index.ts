c31089c82c308f0f12133f1774456b7ebc7d3826
iwanhae <iwanhae@gmail.com>
2020-07-29 05:10:09 +0000

    static serving 추가, 테스트파일 작성필요

--- server/server/index.ts
+++ server/server/index.ts
@@ -4,6 +4,9 @@ import Logger from 'koa-logger';
 import Api, { route } from './api';
 import User from './v1/user';
 
+const serve = require('koa-static');
+const send = require('koa-send');
+
 const app = new Koa();
 const router = new Router();
 
@@ -14,6 +17,10 @@ router.get('/', (ctx: Koa.Context) => {
 router.use('/api', Api.routes());
 router.use('/v1/user', User.routes());
 
+app.use(serve(`${__dirname}/../build`));
+app.use(async (ctx) => {
+  if (ctx.status === 404) await send(ctx, 'index.html', { root: `${__dirname}/../build` });
+});
 app.use(Logger());
 app.use(router.routes());
 
