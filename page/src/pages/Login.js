79f899dce9aabc81fe39f8aa2e3d1656035faeef
DESKTOP-95NK2BV\heeeun <01072417732w@gmail.com>
2020-08-10 23:38:02 +0900

    env 변수 삭제, 해시 함수 알고리즘 구현 완료, await callback 구조 변경

--- page/src/pages/Login.js
+++ page/src/pages/Login.js
@@ -1,48 +1,63 @@
 import React, { useState, useEffect } from 'react';
 import axios from "axios";
-import {Link, Redirect} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Grid, TextField, Button, Typography } from '@material-ui/core';
 
 function Login(){
     // id, password
     const [id,setId]= useState();
     const [pw,setPw]= useState();
-    const [hide, setHide] = useState(false); // hide form or not
 
-    // Main으로 넘겨줄 정보들
-    const [name, setName] = useState("");
-    const [user_number, setUNumber] = useState(-1);
-    const [meta, setMeta] = useState(Date());
+
+    String.prototype.hexEncode = function(){ // string to hex code
+        var hex, i;
+    
+        var result = "";
+        for (i=0; i<this.length; i++) {
+            hex = this.charCodeAt(i).toString(16);
+            result += ("000"+hex).slice(-4);
+        }
+    
+        return result
+    }
+
+    function hashProcess(){
+        const sha256 = require('sha256');
+        const hashed_token = 'hi'; // api로 받기
+
+        // base64
+        let hashed_pw = btoa(pw);
+        // hash sha256
+        hashed_pw = sha256(hashed_pw);
+        // hex encoding
+        hashed_pw = hashed_pw.hexEncode();
+        // hash with token
+        hashed_pw = sha256(hashed_token + hashed_pw);
+
+        return hashed_pw;
+    }
 
     async function setLoginData(e){ // pw 암호화 및 api data 받기
         try{
-            const sha256 = require('sha256');
-            const secret = process.env.REACT_APP_SECRET;
-            const hashed_pw = sha256(pw + secret);
-            
-            await axios.post('/v1/auth', {
-                userId: id,
-                userPw: hashed_pw,
-            }).then((response) => setDataAndMove(response));
+            let hashed_pw = await hashProcess();
+
+            if(hashed_pw){
+                var response = await axios.post('/v1/auth', { // get api data
+                    userId: id,
+                    userPw: hashed_pw,
+                }).catch((e)=>{
+                    console.log(e);
+                })
+
+                //if(response.status===200)
+                    //window.location.href="/home";
+            }
+
         }catch(e){
-            // error 처리 어떻게 할까요?
+            // error 처리 하기
             console.log(e);
         }
     }
-
-    async function setDataAndMove(response){ // api data를 state에 저장, home으로 이동 버튼 띄우기
-        try{
-            setName(response.data.userName);
-            setUNumber(response.data.userNumber);
-            let meta = {create_at : Date(), modified_at : Date()};
-            meta.create_at = response.data.meta.createAt;
-            meta.modified_at = response.data.meta.modifiedAt;
-            setMeta(meta);
-            
-        }catch(e){
-            console.log(e);
-        }
-    };
 
     function changeId(){
         const id = document.querySelector('#userId');
