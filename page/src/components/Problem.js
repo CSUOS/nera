338af8d61f836e36737c7acb0fea637f997eea11
Hyeon <powergee101@gmail.com>
2020-08-20 17:43:08 +0900

    Add states in Assignment.js and Problem.js to use API

--- page/src/components/Problem.js
+++ page/src/components/Problem.js
@@ -3,26 +3,27 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 const Problem = (props) => {
-    const [text, setText] = React.useState(props.info.question_answer[0].answer_content[0]);
+    const [text, setText] = React.useState(props.info.answerContent);
     
-    const handleTextChange = (event) => {
-        setText(event.target.value);
+    const handleTextChange = (value) => {
+        setText(value);
+        props.onEdit(text, props.info.questionId);
     }
 
     return (
         <Grid container className="problem_container" direction="column">
             <Grid container className="problem_description" direction="row" alignItems="flex-start" justify="flex-start">
-                <h6 className="problem_number">{props.number + "."}</h6>
-                <h6 align="left">{props.info["question_content"]}</h6>
+                <h6 className="problem_number">{props.info.questionNumber + "."}</h6>
+                <h6 align="left">{props.info.questionContent}</h6>
             </Grid>
 
-            <h6 className="problem_score" align="right">{props.info["full_score"] + "점"}</h6>
-            {props.image ? <img className="problem_image" src={props.image} alt="Problem Image" /> : null}
+            <h6 className="problem_score" align="right">{props.info.fullScore + "점"}</h6>
+            {/*props.image ? <img className="problem_image" src={props.image} alt="Problem Image" /> : null*/}
 
             <TextField
                 label="답안"
                 margin="normal"
-                required multiline
+                multiline
                 rows={1}
                 rowsMax={10000}
                 value={text}
@@ -30,30 +31,6 @@ const Problem = (props) => {
             </TextField>
         </Grid>
     );
-}
-
-Problem.propTypes = {
-    info: PropTypes.shape({
-        "question_id": PropTypes.number,
-        "question_content": PropTypes.string,
-        "full_score": PropTypes.number,
-        "question_answer": PropTypes.arrayOf(PropTypes.shape({
-            "user_number": PropTypes.number,
-            "question_id": PropTypes.number,
-            "name": PropTypes.string,
-            "answer_content": PropTypes.arrayOf(PropTypes.string),
-            "submitted": PropTypes.bool,
-            "score": PropTypes.number,
-            "meta": {
-                "create_at": PropTypes.instanceOf(Date),
-                "modified_at": PropTypes.instanceOf(Date)
-            }
-        })),
-        "meta": {
-            "create_at": PropTypes.instanceOf(Date),
-            "modified_at": PropTypes.instanceOf(Date)
-        }
-    })
 }
 
 export default Problem;
--- page/src/pages/Assignment.js
+++ page/src/pages/Assignment.js
@@ -8,17 +8,17 @@ import { useHistory } from "react-router-dom";
 
 const Assignment = (props)=>{
     const [info, setInfo] = useState(undefined);
+    const [questions, setQuestions] = useState(undefined);
     const history = useHistory();
 
-    useEffect(()=>{
+    async function getAssignment() {
         let assignId = props.match.params.as_id;
-        console.log(assignId);
+        
         axios.get(`/v1/assignment/${assignId}`, { withCredentials: true })
             .then(res => {
                 setInfo(res.data);
             })
             .catch(err => {
-                console.log(err);
                 const status = err.response.status;
                 if (status === 400 || status === 401) {
                     alert(`과제 정보를 얻는데 실패하였습니다. 잘못된 요청입니다. (${status})`);
@@ -31,6 +31,62 @@ const Assignment = (props)=>{
                 }
                 history.push("/home");
             });
+    }
+
+    async function getQuestions() {
+        let assignId = props.match.params.as_id;
+        
+        axios.get(`/v1/answer/${assignId}`, { withCredentials: true })
+            .then(res => {
+                let quesArr = [];
+                let number = 1;
+                // 백앤드 코드에서 과제 요청에 대한 응답으로
+                // questionId를 반환하지 않는 것으로 확인됨.
+                // 그 문제가 해결된 이후에 quesArr를 알맞게
+                // 설정하는 코드를 작성할 것.
+                /*for (let ques of res.data.answers) {
+                    let processed = {};
+                    processed.answerContent = ques.answerContent;
+                    processed.questionNumber = number++;
+                    processed.questionId = ques.questionId;
+                    processed.fullScore = 1;
+                    quesArr.push(processed);
+                }*/
+                setQuestions(quesArr);
+            })
+            .catch(err => {
+                const status = err.response.status;
+                if (status === 400) {
+                    alert(`답안 정보를 얻는데 실패하였습니다. 잘못된 요청입니다. (${status})`);
+                }
+                else if (status === 401) {
+                    alert(`답안 정보를 얻는데 실패하였습니다. 인증이 실패하였습니다. (${status})`);
+                }
+                else if (status === 403) {
+                    alert(`답안 정보를 얻는데 실패하였습니다. 권한이 없습니다. (${status})`);
+                }
+                else if (status === 404) {
+                    // 단순히 입력한 답안이 없는 경우이므로 오류는 아님.
+                    // 백앤드 코드에서 과제 요청에 대한 응답으로
+                    // questionId를 반환하지 않는 것으로 확인됨.
+                    // 그 문제가 해결된 이후에 quesArr를 알맞게
+                    // 설정하는 코드를 작성할 것.
+                    return;
+                }
+                else if (status === 500) {
+                    alert("내부 서버 오류입니다. 잠시 후에 다시 시도해주세요...");
+                }
+                history.push("/home");
+            })
+    }
+
+    async function fetchData() {
+        await getAssignment();
+        await getQuestions();
+    }
+
+    useEffect(()=>{
+        fetchData();
     }, [props.match.params.as_id])
 
     if (info === undefined)
