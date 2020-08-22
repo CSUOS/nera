85c3be2f5358a415339d8a20d896c1ea2f31733a
Thereisnotruth <thereisnotruth12@gmail.com>
2020-08-23 05:35:54 +0900

    delete api.ts, index.test.ts, auth.test.ts

--- server/server/index.test.ts
+++
@@ -1,8 +0,0 @@
-import request from 'supertest';
-import app from './index';
-
-test('/ GET 요청 작동 확인', async () => {
-  const response = await request(app.callback()).get('/');
-  expect(response.status).toBe(200);
-  expect(response.text).toBe('hello, NERA!');
-});
