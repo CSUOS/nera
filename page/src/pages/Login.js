a7b4f7644557c42dfa0bf2b61d59d180521a22ef
Thereisnotruth <thereisnotruth12@gmail.com>
2020-08-23 05:47:54 +0900

    로그인 api 연동

--- page/src/pages/Login.js
+++ page/src/pages/Login.js
@@ -27,7 +27,7 @@ function Login(){
             // hash password
             let hashed_pw = hashData(hashData(pw));
             // hash result
-            let result = hashData(hashed_token + hashed_pw);
+            let result = hashData(hashed_token.data + hashed_pw);
 
             return result;
         } catch (err) {
@@ -38,27 +38,23 @@ function Login(){
 
     async function setLoginData(e){ // pw 암호화 및 api data 받기
         let hashed_pw = await hashProcess();
-        var response = await axios.post('/v1/login', { // get api data
+        await axios.post('/v1/login', { // get api data
             userId: id,
             userPw: hashed_pw,
-        }, { withCredentials: true }).catch((err)=>alert("예기치 못한 오류가 발생하였습니다.\n추가 정보: " + err));
-
-        console.log(response);
-
-        const status = response.status;
-        const rabumsStatus = response?.data?.message?.slice(response.data.message.length - 3);
-
-        if(status==400 || rabumsStatus == "400"){
-            alert("아이디, 패스워드가 기입되었는지 다시 한 번 확인해주세요.");
-        }else if(status==403 || rabumsStatus == "403"){
-            alert("아이디, 패스워드가 정확히 기입되었는지 다시 한 번 확인해주세요.");
-        }else if(status==500 || rabumsStatus == "500"){
-            alert("내부 서버 오류입니다. 잠시만 기다려주세요.");
-        }
-
-        if (status === 200 && rabumsStatus == undefined) {
-            //window.location.href = "/home";
-        }
+        }, { withCredentials: true })
+        .then(() => {
+            window.location.href = "/home";
+        })
+        .catch((err)=>{
+            const status = err.response.status;
+            if(status == 400){
+                alert("아이디, 패스워드가 기입되었는지 다시 한 번 확인해주세요.");
+            }else if(status == 403){
+                alert("아이디, 패스워드가 정확히 기입되었는지 다시 한 번 확인해주세요.");
+            }else if(status == 500){
+                alert("내부 서버 오류입니다. 잠시만 기다려주세요.");
+            }
+        });
     }
 
     function changeId(){
@@ -85,7 +81,7 @@ function Login(){
             alert("내부 서버 오류로 token을 찾을 수 없습니다. 로그인을 다시 시도해주세요.");
             return;
         }
-        
+
         let response = await axios.get('/v1/cookieTest', { withCredentials: true }).
             catch((err) => alert("예기치 못한 오류가 발생하였습니다.\n추가 정보: " + err));
         const status = response.status;
@@ -97,9 +93,9 @@ function Login(){
         }else if(status==500 || rabumsStatus == "500"){
             alert("내부 서버 오류입니다. 잠시만 기다려주세요.");
         }
-        
+        console.log(response.data);
         if (status == 200 && rabumsStatus == undefined) {
-            history.push("/home");
+           window.location.href = "/home";
            console.log(response);
         }
     }
