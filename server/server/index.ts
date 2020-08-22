7d7b09240067603c589112e5380242e80c40ea64
Thereisnotruth <thereisnotruth12@gmail.com>
2020-08-23 05:39:02 +0900

    try catch 문 삭제, type.ts 추가, vault db 연결 함수 위치 변경

--- server/server/index.ts
+++ server/server/index.ts
@@ -1,7 +1,6 @@
 import Koa from 'koa';
 import Router from 'koa-router';
 import Logger from 'koa-logger';
-import Api, { route } from './api';
 import Login from './v1/login';
 import Logout from './v1/logout';
 import Answer from './v1/answer';
@@ -10,15 +9,14 @@ import Test from './v1/cookieTest'; // 테스트용 쿠키 발급
 import Assignment from './v1/assignment';
 import Token from './v1/token';
 import UserInfo from './v1/userInfo';
-import TTT from './v1/test';
 
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const serve = require('koa-static');
 const send = require('koa-send');
 const cors = require('@koa/cors');
-const { jwtMiddleware, envMiddleware } = require('../config');
-const { isLogin } = require('./v1/login');
+const { jwtMiddleware } = require('../config');
+const { dbMiddleware } = require('./db');
 
 const app = new Koa();
 const router = new Router();
@@ -30,38 +28,28 @@ const corsOption = {
 };
 app.use(cors(corsOption));
 app.context.user = { // 유저 정보 저장
-  _id: 0, // 고유 id
-  userId: '', // 유저 id
-  userName: '', // 유저 이름
-  userNumber: 0, // 학번
+  _id: Number, // 고유 id
+  userId: String, // 유저 id
+  userName: String, // 유저 이름
+  userNumber: Number, // 학번
 };
-/*
-app.context.env = { // 환경변수
-  accessSecretKey: '', // 로그인 토큰 암호화 키
-  mongoAddr: '', // MongoDB 주소
-  rabumsAddr: '', // RABUMS 주소
-  rabumsToken: '', // RABUMS 토큰
-};
-*/
-app.context.role = ''; // 유저 권한 1 - 교수, 2 - 학생
-router.get('/', (ctx: Koa.Context) => {
-  ctx.body = 'hello, NERA!';
-});
+
+app.context.role = String; // 유저 권한 1 - 교수, 2 - 학생
 
 login.use('/v1/login', Login.routes()); // 로그인 필요하지 않은 api
 login.use('/v1/cookieTest', Test.routes());
 login.use('/v1/token', Token.routes());
 
-router.use('/api', Api.routes()); // 로그인이 필요한 api
+// 로그인이 필요한 api
 router.use('/v1/answer', Answer.routes());
 router.use('/v1/student', Student.routes());
 router.use('/v1/assignment', Assignment.routes());
 router.use('/v1/logout', Logout.routes());
 router.use('/v1/userInfo', UserInfo.routes());
-router.use('/v1/test', TTT.routes());
 
 app.use(Logger());
 
+app.use(dbMiddleware);
 app.use(login.routes());
 app.use(jwtMiddleware);
 app.use(router.routes());
