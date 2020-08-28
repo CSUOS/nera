1ab5ea650ea3670281026952f20986d3746f22a1
Thereisnotruth <thereisnotruth12@gmail.com>
2020-08-28 15:19:50 +0900

    delete unnecessary file, code - index.ts, test.ts

--- server/server/index.ts
+++ server/server/index.ts
@@ -2,21 +2,13 @@ import Koa from 'koa';
 import Router from 'koa-router';
 import Logger from 'koa-logger';
 import Login from './v1/login';
-import Logout from './v1/logout';
-import Answer from './v1/answer';
-import Student from './v1/student';
 import Test from './v1/cookieTest'; // 테스트용 쿠키 발급
-import Assignment from './v1/assignment';
 import Token from './v1/token';
-import UserInfo from './v1/userInfo';
 import AuthCheck from './v1/authCheck';
-
-const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const serve = require('koa-static');
 const send = require('koa-send');
 const cors = require('@koa/cors');
-const { jwtMiddleware } = require('../config');
 const { dbMiddleware } = require('./db');
 
 const app = new Koa();
@@ -42,19 +34,11 @@ login.use('/v1/cookieTest', Test.routes());
 login.use('/v1/token', Token.routes());
 
 // 로그인이 필요한 api
-/*
-router.use('/v1/answer', Answer.routes());
-router.use('/v1/student', Student.routes());
-router.use('/v1/assignment', Assignment.routes());
-router.use('/v1/logout', Logout.routes());
-router.use('/v1/userInfo', UserInfo.routes());
-*/
 router.use('/v1', AuthCheck.routes());
 app.use(Logger());
 
 app.use(dbMiddleware);
 app.use(login.routes());
-// app.use(jwtMiddleware);
 app.use(router.routes());
 
 app.use(serve(`${__dirname}/../build`));
