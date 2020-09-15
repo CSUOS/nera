f6f2ead07b8ff6450a4e5b3bbd44beb4e3930479
heeeun <01072417732w@gmail.com>
2020-09-16 00:34:01 +0900

    Home.js에서도 state에 따른 색상 맞추기

--- page/src/components/AssignmentBox.js
+++ page/src/components/AssignmentBox.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import {useAssignmentState} from '../shared/AssignmentState';
 
 import {Link} from 'react-router-dom';
 import { Grid, Paper } from '@material-ui/core';
@@ -17,22 +18,25 @@ const AssignmentBox = (props)=>{
   let color="black";
   let state_word = "error";
 
+  const asState = useAssignmentState();
+
   if(type===0){
     switch(state){
-      case 0: state_word="공개 전"; color = "rgb(56, 142, 60)"; break;
-      case 1: state_word="진행 중"; color = "#f50057"; break;
-      case 2: state_word="마감 됨"; color = "black"; break;
+      case asState["notReleased"]: state_word="공개 전"; color = "black"; break; // 검
+      case asState["released"]: state_word="진행 중"; color = "rgb(56, 142, 60)"; break; // 초
+      case asState["scoring"]: state_word="채점 필요"; color = "#f50057"; break; // 빨
+      case asState["done"]: state_word="마감 됨"; color = "#3f51b5"; break; // 파
     }
   }else if(type===1){
     switch(state){
-      case 0: state_word="공개 전"; color = "#f50057"; break;
-      case 1: state_word="제출 필요"; color = "rgb(56, 142, 60)"; break;
-      case 2: state_word="채점 중"; color = "#3f51b5"; break;
-      case 3: state_word="채점 완료"; color = "black"; break;
+      case asState["notReleased"]: state_word="공개 전"; color = "black"; break;
+      case asState["released"]: state_word="제출 필요"; color = "#f50057"; break;
+      case asState["scoring"]: state_word="채점 중"; color = "rgb(56, 142, 60)"; break;
+      case asState["done"]: state_word="채점 완료"; color = "#3f51b5"; break;
     }
   }
   const useStyles = makeStyles((theme) => ({
-    circleSytle : {
+    stateSytle : {
       background: color,
       padding: '5px 10px',
       color: 'white'
@@ -47,7 +51,7 @@ const AssignmentBox = (props)=>{
       <Link to ={`/home/assignment/${as_info.assignmentId}`}>
             <Grid className="a_box_header">
               <Grid className="a_box_title">{as_info.assignmentName}</Grid>
-              <Grid className={classes.circleSytle}>{state_word}</Grid>
+              <Grid className={classes.stateSytle}>{state_word}</Grid>
             </Grid>
             <Grid className="a_box_deadline">{deadline} 까지</Grid>
       </Link>
